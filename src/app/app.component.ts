ffc2b525a5e43af1f0a6c66c2253cf6c10987ebd
ravindraparcha <mr.ravindraparcha@gmail.com>
2018-06-17 10:49:11 +0530

    validation added for 80G and css issue fix for modal popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, ViewContainerRef } from '@angular/core';
- 
-
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'//,
   //styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'app';
+
+   
+  ngOnInit() {
+    
+    
+
+    $(window).scroll(function () {
+      var stickyNavTop = $('#navbar-header').offset().top;
+      if ($(window).scrollTop() > stickyNavTop) {
+        $('#navbar-header').addClass('fixed');
+      }
+      else {
+        $('#navbar-header').removeClass('fixed');
+      }
+    });
+  }
 }
