6c65e4567033e1f84fb8ed119ecd8c82f6aad6e6
Ravindra Parcha <ravindrap@citiustech.com>
2018-07-04 12:01:29 +0530

    NA if 'Any Other' selected as nature of income

--- src/app/modules/eTaxXML/directives/update-next-field.directive.ts
+++ src/app/modules/eTaxXML/directives/update-next-field.directive.ts
@@ -14,17 +14,32 @@ import { NG_VALIDATORS, Validator, AbstractControl } from "@angular/forms";
 
 export class UpdateNextFieldDirective implements Validator {
     @Input() indexValue: number;
-    constructor(@Attribute('nextField') private _nextField: string, @Attribute('percentage') private _percentage: number) { }
+    constructor( @Attribute('nextField') private _nextField: string, @Attribute('percentage') private _percentage: number,@Attribute('setValue') private _setValue : string) { }
     public validate(control: AbstractControl) {
+        debugger;
         let nextField = control.root.get(this._nextField + '_' + this.indexValue);
-        if (control.value == undefined || control.value == null || control.value=='') {            
-            if(nextField != null)
-                nextField.setValue(0);
+        if (control.value == undefined || control.value == null || control.value == '') {
+            if (nextField != null)
+                if(this._percentage!=null)
+                    nextField.setValue(0);
+                else if(this._setValue!=null)
+                    nextField.setValue('');
             return;
-        } 
-        if (!isNaN(control.value) && nextField != null) {
-            nextField.setValue(Math.ceil((parseInt(control.value) * this._percentage) / 100));
+        }
+        if (this._percentage != null) {
+            if (!isNaN(control.value) && nextField != null) {
+                nextField.setValue(this.calcualtePercentage(parseInt(control.value), this._percentage));
+            }
+        }
+        if(this._setValue!=null) {
+            if(control.value=="OTH") 
+                nextField.setValue(this._setValue); 
+            else 
+                nextField.setValue('');               
         }
         return null;
+    }
+    private calcualtePercentage(inputValue: number, percentage: number) {
+        return Math.ceil((inputValue * percentage) / 100);
     }
 }
