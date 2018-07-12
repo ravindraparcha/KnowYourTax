d9802feb9c60c584f0782b3c4c706294943d7dc1
Ravindra Parcha <ravindrap@citiustech.com>
2018-07-12 10:41:08 +0530

    message added on assessment year change

--- src/app/modules/shared/components/assessmentYear/ay.component.ts
+++ src/app/modules/shared/components/assessmentYear/ay.component.ts
@@ -1,7 +1,7 @@
 
 import { Component,OnInit } from '@angular/core';
 import { ConfigurationService } from '../../../shared/services/configurationService';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
     selector: 'assessment-year',
     templateUrl: './ay.component.html'
@@ -9,7 +9,7 @@ import { ConfigurationService } from '../../../shared/services/configurationServ
 export class AYComponent implements OnInit {
     public assessmentYearList;
     public selectedAssessmentYear;
-    constructor(private _configurationService: ConfigurationService) {
+    constructor(private _configurationService: ConfigurationService,private _toastr: ToastrService) {
         this.assessmentYearList = [];
         this.selectedAssessmentYear = '';
     }
@@ -17,18 +17,22 @@ export class AYComponent implements OnInit {
         this._configurationService.masterSec.forEach(element => {
             this.assessmentYearList.push({ 'value': element.ayYear, 'text': element.ayYear });
         });
-        let currentDate = new Date();
-        let currentYear = currentDate.getFullYear();
-        let nextYear = new Date(currentDate.getFullYear() + 1, 1, 1).getFullYear();
-        this.selectedAssessmentYear = currentYear + '-' + nextYear;
-        this.changeAssessmentYearData(this.selectedAssessmentYear);
-    }
+        this.selectedAssessmentYear =this.getCurrentAssessmentYear()
+        this.changeAssessmentYearData( this.selectedAssessmentYear,true);
+    }    
     public resetAssessmentYear(): void {
         this.selectedAssessmentYear = '';
     }
 
-    public changeAssessmentYearData(selectedAYYear: string): void {
+    private getCurrentAssessmentYear() : string {
+        let currentDate = new Date();
+        let currentYear = currentDate.getFullYear();
+        let nextYear = new Date(currentDate.getFullYear() + 1, 1, 1).getFullYear();
+        return (currentYear + '-' + nextYear);
+    }
 
+    public changeAssessmentYearData(selectedAYYear: string,isTriggered:boolean =false): void {
+        let isDone:boolean = false;
         this._configurationService.selectedMasterSec = [];
         this._configurationService.selectedSlabs = [];
         if (selectedAYYear == null)
@@ -37,14 +41,22 @@ export class AYComponent implements OnInit {
         this._configurationService.masterSec.forEach(element => {
             if (element.ayYear === selectedAYYear) {
                 this._configurationService.selectedMasterSec.push(element);
+                isDone = true;
                 return false;
             }
         });
         this._configurationService.slabs.forEach(element => {
             if (element.ayYear === selectedAYYear) {
                 this._configurationService.selectedSlabs.push(element);
+                isDone=true;
                 return false;
             }
         });
+        if(isDone && !isTriggered)
+            this._toastr.success('Assessment year changed successfully', 'Success', this._configurationService.CustomToastOptions);
+        else if(!isDone){
+            this._toastr.error('Oops! Some error has occurred', 'Error', this._configurationService.CustomToastOptions);
+            this.selectedAssessmentYear = this.getCurrentAssessmentYear();
+        }        
     }
 }
